5acdea773c71e763c85c0de9b8f3994ee23eb234
inf103461 <64414543+inf103461@users.noreply.github.com>
2024-01-10 16:37:34 +0000

    Finished ex9

--- Assignments/Exercise09/clock.erl
+++ Assignments/Exercise09/clock.erl
@@ -1,11 +1,28 @@
 -module(clock).
--export([start/1, get/1, tick/2, test_clock/0]).
-
--define(MAX_INT, 999999999).
+-export([start/1, get/1, tick/2, test_all/0, test_clock/0, test_get/0, test_tick/0]).
 
 % Logging
 log_enabled() -> true.
 
+-define(PID_LENGTH, 9).
+-define(SOURCE_LENGTH, 5).
+
+log(Pid, Source, Entry, Args) ->
+    case log_enabled() of
+        true ->
+            PidStr = io_lib:format("~p", [Pid]),
+            PidStr = pad_string(PidStr, ?PID_LENGTH - string:length(PidStr)),
+            SourceStr = pad_string(Source, ?SOURCE_LENGTH - string:length(Source)),
+
+            io:format(PidStr ++ ", " ++ SourceStr ++ ": " ++ Entry ++ "~n", Args);
+        false -> ok
+    end.
+
+pad_string(String, Amount) when Amount > 0 -> pad_string(" " ++ String, Amount - 1);
+pad_string(String, _) -> String.
+
+
+% Unformatted logging
 log(Format, Args) ->
     case log_enabled() of
         true -> io:format(Format, Args);
@@ -13,44 +30,70 @@ log(Format, Args) ->
     end.
 
 
-start(Interval) ->
-    log("~p,   MAIN: Starting clock(~p)~n", [self(), Interval]),
-    spawn(fun() -> clock(0, Interval, 0) end).
+% Tick config
+enable_tick(Enabled) when is_boolean(Enabled) ->
+    erlang:put(enable_tick, Enabled).
 
-
-clock(Time, Interval, PauseInterval) ->
-    log("~p,  CLOCK: New clock cycle, Time: ~p~n", [self(), Time]),
-    receive
-        {set, Value} ->
-            log("~p,  CLOCK: {set, ~p}~n", [self(), Value]),
-            clock(Value, Interval, PauseInterval);
-
-        {get, Pid} ->
-            log("~p,  CLOCK: {get, ~p}~n", [self(), Pid]),
-            Pid ! {clock, Time};
-
-        pause ->
-            log("~p,  CLOCK: pause~n", [self()]),
-            clock(Time, ?MAX_INT, Interval);
-
-        resume ->
-            log("~p,  CLOCK: resume~n", [self()]),
-            clock(Time, PauseInterval, 0);
-
-        stop ->
-            log("~p,  CLOCK: stop~n", [self()]),
-            ok;
-        tick ->
-            log("~p,  CLOCK: received tick~n", [self()]),
-            clock(Time + Interval, Interval, PauseInterval)
-    after Interval -> clock(Time + Interval, Interval, PauseInterval)
+tick_enabled() ->
+    case erlang:get(enable_tick) of
+        undefined -> false; % Default value
+        Value -> Value
     end.
 
+
+% Main code
+start(Interval) ->
+    log(self(), "MAIN", "Starting clock(~p)", [Interval]),
+    case tick_enabled() of
+        false ->
+            Clock = spawn(fun() -> clock(0, Interval, false) end),
+            {Clock, undefined};
+        true -> 
+            Clock = spawn(fun() -> clock_ticked(0, false) end),
+            log(self(), "MAIN", "Starting ticker(~p)", [Interval]),
+            Ticker = spawn(fun() -> tick(Interval, Clock) end),
+            {Clock, Ticker}
+    end.
+
+
+clock(Time, Interval, Paused) ->
+    log(self(), "CLOCK", "New clock cycle, Time: ~p", [Time]),
+    receive
+        {set, Value} ->
+            log(self(), "CLOCK", "{set, ~p}", [Value]),
+            clock(Value, Interval, Paused);
+
+        {get, Pid} ->
+            log(self(), "CLOCK", "{get, ~p}", [Pid]),
+            Pid ! {clock, Time},
+            clock(Time, Interval, Paused);
+
+        pause ->
+            log(self(), "CLOCK", "pause", []),
+            clock(Time, Interval, true);
+
+        resume ->
+            log(self(), "CLOCK", "resume", []),
+            clock(Time, Interval, false);
+
+        stop ->
+            log(self(), "CLOCK", "stop", []),
+            ok
+
+    after Interval ->
+        case Paused of
+            true -> clock(Time, Interval, Paused);
+            false -> clock(Time + Interval, Interval, Paused)
+        end
+    end.
+
+
 get(Pid) ->
-    log("~p, GETTER: {get, ~p}~n", [self(), Pid]),
+    log(self(), "MAIN", "{get, ~p}", [Pid]),
     Pid ! {get, self()},
     receive
-        {clock, Time} -> Time
+        {clock, Time} -> log(self(), "MAIN", "Received time: ~p", [Time]),
+        Time
     end.
 
 % Disadvantage: Runtime of code in receive-block influences clock time.
@@ -58,21 +101,119 @@ get(Pid) ->
 % cycle with the same time value to start, increasing time of the new
 % cycle by the remainder of the last.
 tick(Interval, Pid) ->
-    log("~p,   TICK: sending tick~n", [self()]),
-    Pid ! tick,
-    timer:sleep(Interval),
-    tick(Interval, Pid).
+    log(self(), "TICKER", "sending tick", []),
+    Pid ! {tick, Interval},
+    receive
+        stop ->
+            log(self(), "TICKER", "stop", []),
+            ok
+    after Interval -> tick(Interval, Pid)
+    end.
+
+clock_ticked(Time, Paused) ->
+        log(self(), "CLOCK", "New clock cycle, Time: ~p", [Time]),
+    receive
+        {set, Value} ->
+            log(self(), "CLOCK", "{set, ~p}", [Value]),
+            clock_ticked(Value, Paused);
+
+        {get, Pid} ->
+            log(self(), "CLOCK", "{get, ~p}", [Pid]),
+            Pid ! {clock, Time},
+            clock_ticked(Time, Paused);
+
+        pause ->
+            log(self(), "CLOCK", "pause", []),
+            clock_ticked(Time, true);
+
+        resume ->
+            log(self(), "CLOCK", "resume", []),
+            clock_ticked(Time, false);
+
+        stop ->
+            log(self(), "CLOCK", "stop", []),
+            ok;
+
+        {tick, TimeSinceLast} ->
+            log(self(), "CLOCK", "received tick", []),
+            case Paused of
+                true -> clock_ticked(Time, Paused);
+                false -> clock_ticked(Time + TimeSinceLast, Paused)
+            end
+    end.
 
 
+% Testing (make sure logging is enabled)
+-define(TEST_CLOCK_INTERVAL, 1000).
+-define(TEST_DEFAULT_WAIT, 3500).
+-define(TEST_WAIT_INBETWEEN, 2000).
+
+test_all() -> 
+    log("~nRUNNING ALL TESTS: ~n", []),
+    
+    log("====================== ~n~n", []),
+    log("RUNNING CLOCK TESTS: ~n", []),
+    log("================================================= ~n", []),
+    test_clock(),
+    timer:sleep(?TEST_WAIT_INBETWEEN),
+
+    log("================================================= ~n~n", []),
+    log("RUNNING GET TESTS: ~n", []),
+    log("================================================= ~n", []),
+    test_get(),
+    timer:sleep(?TEST_WAIT_INBETWEEN),
+
+    log("================================================= ~n~n", []),
+    log("RUNNING TICK TESTS: ~n", []),
+    log("================================================= ~n", []),
+    test_tick(),
+    timer:sleep(?TEST_WAIT_INBETWEEN),
+
+    log("================================================= ~n", []),
+    enable_tick(false),
+    ok.
 
 test_clock() ->
-    Clock = start(1000),
-    timer:sleep(5500),
+    enable_tick(false),
+    {Clock, _} = start(?TEST_CLOCK_INTERVAL),
+    timer:sleep(?TEST_DEFAULT_WAIT),
+
     Clock!pause,
-    timer:sleep(5500),
+    timer:sleep(?TEST_DEFAULT_WAIT),
+
     Clock!resume,
-    timer:sleep(5500),
-    Clock!{set, 1000000000},
-    timer:sleep(5500),
+    timer:sleep(?TEST_DEFAULT_WAIT),
+
+    Clock!{set, 10000000},
+    timer:sleep(?TEST_DEFAULT_WAIT),
+
     Clock!stop,
+    ok.
+
+test_get() ->
+    enable_tick(false),
+    {Clock, _} = start(?TEST_CLOCK_INTERVAL),
+    timer:sleep(?TEST_DEFAULT_WAIT),
+    Time = clock:get(Clock), % specify module because get() is ambiguous
+    log(self(), "MAIN", "Extracted time: ~p", [Time]),
+    timer:sleep(?TEST_DEFAULT_WAIT),
+    Clock!stop,
+    ok.
+
+test_tick() ->
+    enable_tick(true),
+    {Clock, Ticker} = start(?TEST_CLOCK_INTERVAL),
+    timer:sleep(?TEST_DEFAULT_WAIT),
+
+    Clock!pause,
+    timer:sleep(?TEST_DEFAULT_WAIT),
+
+    Clock!resume,
+    timer:sleep(?TEST_DEFAULT_WAIT),
+
+    Clock!{set, 10000000},
+    timer:sleep(?TEST_DEFAULT_WAIT),
+
+    Clock!stop,
+    Ticker!stop,
     ok.
